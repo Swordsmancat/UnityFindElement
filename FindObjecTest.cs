f4db0e9f4e10d5b4a14894c5b3cbe3c34639d1dd
Swordsmancat <569558422@qq.com>
2021-07-03 13:53:48 +0800

    Add files via upload

---
+++ FindObjecTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Reflection;
+
+public class FindObjecTest : MonoBehaviour
+{
+
+    public virtual void Awake()
+    {
+        var fields = GetType().GetFields(BindingFlags.Instance | BindingFlags.Public);
+        foreach (var field in fields)
+        {
+            var name = field.Name;
+            Transform obj = FindDeep(transform, name);
+
+            if(obj != null)
+            {
+                if (typeof(GameObject).Equals(field.FieldType))
+                {
+                    field.SetValue(this, obj.gameObject);
+                }
+                else
+                {
+                    field.SetValue(this, obj.GetComponent(field.FieldType));
+                }
+            }
+        }
+    }
+
+    private Transform FindDeep(Transform t, string childName)
+    {
+        if (t.name.Equals(childName))
+            return t;
+        for (int i = 0; i < t.childCount; i++)
+        {
+            var child = FindDeep(t.GetChild(i), childName);
+            if (child) return child;
+        }
+        return null;
+    }
+}
